ee441c9d0962132a72a94e710cb47395a29d1e19
Oleg Sidorov <oleg@sidorov.nl>
2025-11-04 22:12:22 +0100

    Update highlights file

--- languages/liss/highlights.scm
+++ languages/liss/highlights.scm
@@ -2,6 +2,7 @@
 (kw_fn) @keyword
 (kw_let) @keyword
 (kw_cond) @keyword
+(kw_import) @keyword
 
 ; Literals
 (string) @string
@@ -15,8 +16,23 @@
 ; Operators
 (operator) @operator
 
+; Punctuation
+"?" @punctuation.special
+
 ; Function definitions
 (sexp (kw_fn) (identifier) @function)
 
 ; Function calls
 (sexp (identifier) @function)
+
+; For qualified identifiers like module:name
+(qualified_identifier
+  module: (identifier) @namespace)
+
+; For function calls with qualified identifiers like (module:name ...)
+(sexp
+  (qualified_identifier
+    function: (identifier) @function))
+
+; Comments
+(comment) @comment
